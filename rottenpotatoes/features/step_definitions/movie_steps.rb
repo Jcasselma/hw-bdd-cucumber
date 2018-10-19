be3981db8de0b78eb6b03eee5b894a3408eea730
EC2 Default User <ec2-user@ip-172-31-26-241.us-east-2.compute.internal>
2018-10-19 02:24:34 +0000

    completion

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,10 +2,8 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -16,9 +14,7 @@ end
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  page.should have_content(/#{e1}.*#{e2}/)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +25,19 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(" ").each do |ratings|
+    step "I #{uncheck}check \"ratings_#{ratings}\""
+  end
+    
 end
 
-Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+When /I should (not )?see the following movies: (.*)/ do |shouldnot, movie_list|
+  movie_list.split(", ").each do |movies|
+    step "I should #{shouldnot}see #{movies}"
+  end
+    
+end
+
+Then /I should see all the movies/ do 
+  page.all(:css, "movies tr").count == 10
 end
